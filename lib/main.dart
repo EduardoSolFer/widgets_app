fe9b86bec9169af566b6613656c181a34f9c201b
fmb2021 <gerardo.mendoza@fmbsolutions.mx>
2024-02-15 01:01:06 -0600

    Se creó el homeScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/theme/app_theme.dart';
+import 'presentation/screens/home/home_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +11,16 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false, //Oculta la cinta de debuggin
+      theme: AppTheme(selectedColor: 5).getTheme(),
+      home: const HomeScreen() 
+      
+      // Scaffold(
+      //   body: Center(
+      //     child: FilledButton(onPressed: (){}, child:  Text('Hola Mundo de prueba!')),
+        // ),
+      // ),
     );
   }
 }
